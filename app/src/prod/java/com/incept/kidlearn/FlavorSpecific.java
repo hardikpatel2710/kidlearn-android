f9d159537801f52a3946abbbd6c5ff938e1cb744
Hardik Patel <hardik.patel@mindstix.com>
2018-03-30 16:13:01 +0530

    Initial commit. Profile screen is done. Navigation drawer is in progress.

---
+++ app/src/prod/java/com/incept/kidlearn/FlavorSpecific.java
@@ -0,0 +1,15 @@
+package com.incept.kidlearn;
+
+/**
+ * FlavorSpecific class for 'Prod' build variants.
+ * This class will hold all the required configuration for running 'Prod' variant of the application.
+ * Example: Analytics Keys, Crash Reporting Tool Keys, Push Notification Platform Configuration etc.
+ *
+ */
+
+public class FlavorSpecific {
+
+    // Application level lof tag.
+    public static final String APP_LOG_TAG = "KidLearn Prod";
+
+}
